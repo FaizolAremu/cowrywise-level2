a77d54faaf02a3183ad7bae7c1d45fd5f33da068
Faizol Aremu <waliufaizol@gmail.com>
2026-06-10 12:53:03 -0700

    Update signup form to correctly store user details in local storage

--- script.js
+++ script.js
@@ -10,7 +10,34 @@ const confirmPasswordInput = document.getElementById("confirmPassword");
 const errorMessage = document.getElementById("error");
 
 
-let users = JSON.parse(localStorage.getItem("users")) || [];
+let users = JSON.parse(localStorage.getItem("usersDetails")) || [];
+
+signupForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let firstNameValue = firstNameInput.value.trim();
+    let lastNameValue = lastNameInput.value.trim();
+    let usernameValue = usernameInput.value.trim();
+    let emailValue = emailInput.value.trim();
+    let phoneValue = phoneInput.value.trim();
+    let passwordValue = passwordInput.value.trim();
+    let confirmPasswordValue = confirmPasswordInput.value.trim();
+
+
+    let newUser = {
+        firstName: firstNameValue,
+        lastName: lastNameValue,
+        username: usernameValue,
+        email: emailValue,
+        phone: phoneValue,
+        password: passwordValue,
+        confirmPassword: confirmPasswordValue,
+    };
+
+    users.push(newUser);
+
+    localStorage.setItem("users", JSON.stringify(users));
+});
 
 
 
